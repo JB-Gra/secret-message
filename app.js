018410b1c6707adea3be1b473ab8801c2aa572ac
JB-Gra <timishayd@gmail.com>
2024-02-18 19:53:50 -0600

    Se modificó app.js para que mostrara el valor del área de texto en pantalla, se ajustaron detalles al styles.css

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const entradaTexto = document.getElementById("entrada-texto");
 const resultadoTexto = document.getElementById("resultado-texto");
 const btnEncriptado = document.getElementById("encriptado");
 const btnDesencriptado = document.getElementById("desencriptado");
+const areaMensaje = document.getElementById("resultado-texto");
+const mensajeEspera = document.getElementById("esperando-mensaje");
+let textoNuevo = document.createElement("p");
 
 // Paso 2: asegurarse de que estemos capturando el valor del área de texto
-btnEncriptado.addEventListener("click", () => alert(entradaTexto.value))
+// Paso 3: mostrar el texto en pantalla
+// Paso 3.5: reemplazar el texto una vez que se crea
+const crearMensaje = () => {
+  textoNuevo.classList.add("salida__resultado-texto");
+  textoNuevo.textContent = entradaTexto.value.toLowerCase();
+  areaMensaje.append(textoNuevo);
+}
+
+btnEncriptado.addEventListener("click", () => {
+  mensajeEspera.remove();
+  textoNuevo.remove();
+  crearMensaje();
+})
