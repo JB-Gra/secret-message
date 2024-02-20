c9ca721f689d6814356b6f8e5144c59dccb2f154
JB-Gra <timishayd@gmail.com>
2024-02-19 19:53:33 -0600

    Funciones de encriptado, desencriptado y copiado de texto añadidos a app.js, además de la visualización en la interfaz

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-// Paso 1: tomar los elementos del DOM (área de texto, resultado del mensaje y botones)
 const entradaTexto = document.getElementById("entrada-texto");
 const resultadoTexto = document.getElementById("resultado-texto");
 const btnEncriptado = document.getElementById("encriptado");
 const btnDesencriptado = document.getElementById("desencriptado");
 const areaMensaje = document.getElementById("resultado-texto");
 const mensajeEspera = document.getElementById("esperando-mensaje");
+const btnCopiar = document.getElementById("btn-copiar");
 let textoNuevo = document.createElement("p");
-
-// Paso 2: asegurarse de que estemos capturando el valor del área de texto
-// Paso 3: mostrar el texto en pantalla
-// Paso 3.5: reemplazar el texto una vez que se crea
-const crearMensaje = () => {
-  textoNuevo.classList.add("salida__resultado-texto");
-  textoNuevo.textContent = entradaTexto.value.toLowerCase();
-  areaMensaje.append(textoNuevo);
-}
 
 btnEncriptado.addEventListener("click", () => {
   mensajeEspera.remove();
   textoNuevo.remove();
-  crearMensaje();
+  crearMensaje(encriptarMensaje(entradaTexto.value));
+  entradaTexto.value = "";
 })
+
+btnDesencriptado.addEventListener("click", () => {
+  mensajeEspera.remove();
+  textoNuevo.remove();
+  crearMensaje(desencriptarMensaje(entradaTexto.value));
+  entradaTexto.value = "";
+})
+
+btnCopiar.addEventListener("click", () => {
+  let texto = textoNuevo.textContent;
+  navigator.clipboard.writeText(`${texto}`).then(() => {
+    console.log("¡Texto copiado!");
+  }, () => {
+    console.error("Hubo un error al copiar");
+  })
+})
+
+const crearMensaje = (func) => {
+  textoNuevo.classList.add("salida__resultado-texto");
+  textoNuevo.textContent = func;
+  areaMensaje.append(textoNuevo);
+}
+
+const encriptarMensaje = (mensaje) => {
+  const vocales = {
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
+  }
+  const mensajeEncriptado = mensaje.replace(/(a|e|i|o|u)/g, (vocal) => {
+    return vocales[vocal];
+  })
+  return mensajeEncriptado;
+}
+
+const desencriptarMensaje = (mensaje) => {
+  const encriptado = {
+    "ai": "a",
+    "enter": "e",
+    "imes": "i",
+    "ober": "o",
+    "ufat": "u",
+  }
+  const mensajeDesencriptado = mensaje.replace(/(ai|enter|imes|ober|ufat)/g, (frase) => {
+    return encriptado[frase];
+  })
+  return mensajeDesencriptado;
+}
